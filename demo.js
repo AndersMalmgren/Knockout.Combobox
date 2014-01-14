77eec12b95da3657e01e7c3be4c7461bce907d2a
andresmoschini <andresmoschini@gmail.com>
2014-01-14 10:13:24 -0300

    Add demo for promise `dataSource` function

--- demo.js
+++ demo.js
@@ -37,6 +37,27 @@ ViewModel.prototype = {
         }
 
         return { data: data, total: options.total };
+    },
+    getDataPromise: function (options) {
+        if (options.page == 0) {
+            options.total = Math.floor((Math.random() * 200) + 1);
+        }
+
+        var data = [];
+        for (var i = 0; i < Math.min(options.pageSize, options.total) ; i++) {
+            var index = (i + (options.page * options.pageSize));
+            if (index == options.total) { break; }
+            data.push({ name: options.text + " " + index });
+        }
+
+
+        var deferred = Q.defer();
+        //Simulate async
+        var delay = Math.floor((Math.random() * 100) + 10);;
+        setTimeout(function () {
+            deferred.resolve({ data: data, total: options.total });
+        }, delay);
+        return deferred.promise;
     }
 };
 
