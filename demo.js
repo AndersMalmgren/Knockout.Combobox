6eaf2b0444d9c40a390c34d0c8fc87654e169324
andresmoschini <andresmoschini@gmail.com>
2014-01-14 10:13:05 -0300

    Add demo for synchronous `dataSource` function

--- demo.js
+++ demo.js
@@ -23,6 +23,20 @@ ViewModel.prototype = {
         setTimeout(function () {
             options.callback({ data: data, total: options.total });
         }, delay);
+    },
+    getDataSync: function (options) {
+        if (options.page == 0) {
+            options.total = Math.floor((Math.random() * 200) + 1);
+        }
+
+        var data = [];
+        for (var i = 0; i < Math.min(options.pageSize, options.total) ; i++) {
+            var index = (i + (options.page * options.pageSize));
+            if (index == options.total) { break; }
+            data.push({ name: options.text + " " + index });
+        }
+
+        return { data: data, total: options.total };
     }
 };
 
